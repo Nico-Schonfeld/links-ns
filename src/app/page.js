2e13e51a8670532fe0cf1253e8a170abaa1f31b6
Nico-Schonfeld <nicoschonfeld88@gmail.com>
2023-12-19 20:10:27 -0300

    links-ns

---
+++ src/app/page.js
@@ -0,0 +1,14 @@
+"use client";
+
+import Home from "@/components/Home";
+import { useState } from "react";
+
+const App = () => {
+  return (
+    <>
+      <Home />
+    </>
+  );
+};
+
+export default App;
